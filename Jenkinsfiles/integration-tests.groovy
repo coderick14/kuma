029943094db69f56ba6d12e47b1927bd7f8b078a
John Whitlock <jwhitlock@mozilla.com>
2017-11-17 11:27:46 -0600

    bug 1402950: Add Chrome to integration tests
    Add Chrome functional tests to the integration tests, and run them in
    parallel.

--- Jenkinsfiles/integration-tests.groovy
+++ Jenkinsfiles/integration-tests.groovy
@@ -6,41 +6,57 @@ stage('Build') {
   }
 }
 
-stage('Functional') {
-  def pwd = pwd()
-  def cmd = "py.test tests/functional" +
-            " --driver Remote" +
-            " --capability browserName firefox" +
-            " --host hub" +
-            " --base-url='${config.job.base_url}'" +
-            " --junit-xml=/test_results/integration.xml"
-  if (config.job && config.job.tests) {
-    cmd += " -m \"${config.job.tests}\""
-  }
-  if (config.job && config.job.maintenance_mode) {
-    cmd += " --maintenance-mode"
-  }
 
-  dockerRun("selenium/hub:${config.job.selenium}",
-            ["docker_args": "--name selenium-hub-${BUILD_TAG}"]) {
-    dockerRun("selenium/node-firefox:${config.job.selenium}",
-              ["docker_args": "--link selenium-hub-${BUILD_TAG}:hub",
-               "copies": config.job.selenium_nodes]) {
-      dockerRun("kuma-integration-tests:${GIT_COMMIT_SHORT}",
-                ["docker_args": "--link selenium-hub-${BUILD_TAG}:hub" +
-                                " --volume ${pwd}/test_results:/test_results" +
-                                " --user 1000",
-                 "cmd": cmd])
+def functional_test(browser, base_dir) {
+  return {
+    node {
+      def cmd = "py.test tests/functional" +
+                " --driver Remote" +
+                " --capability browserName ${browser}" +
+                " --host hub" +
+                " --base-url='${config.job.base_url}'" +
+                " --junit-xml=/test_results/functional-${browser}.xml"
+      def hubname = "selenium-hub-for-${browser}-${BUILD_TAG}"
+      if (config.job && config.job.tests) {
+        cmd += " -m \"${config.job.tests}\""
+      }
+      if (config.job && config.job.maintenance_mode) {
+        cmd += " --maintenance-mode"
+      }
+      dockerRun("selenium/hub:${config.job.selenium}",
+                ["docker_args": "--name ${hubname}"]) {
+        dockerRun("selenium/node-${browser}:${config.job.selenium}",
+                  ["docker_args": "--link ${hubname}:hub",
+                   "copies": config.job.selenium_nodes]) {
+          dockerRun("kuma-integration-tests:${GIT_COMMIT_SHORT}",
+                    ["docker_args": "--link ${hubname}:hub" +
+                                    " --volume ${base_dir}/test_results:/test_results" +
+                                    " --user 1000",
+                     "cmd": cmd])
+        }
+      }
     }
   }
 }
 
-stage('Headless') {
-  def pwd = pwd()
-  dockerRun("kuma-integration-tests:${GIT_COMMIT_SHORT}",
-            ["docker_args": "--volume ${pwd}/test_results:/test_results" +
-                            " --user 1000",
-             "cmd": "py.test tests/headless" +
-                    " --base-url='${config.job.base_url}'" +
-                    " --junit-xml=/test_results/headless.xml"])
+def headless_test(base_dir) {
+  return {
+    node {
+      dockerRun("kuma-integration-tests:${GIT_COMMIT_SHORT}",
+                  ["docker_args": "--volume ${base_dir}/test_results:/test_results" +
+                                  " --user 1000",
+                  "cmd": "py.test tests/headless" +
+                          " --base-url='${config.job.base_url}'" +
+                          " --junit-xml=/test_results/headless.xml"])
+    }
+  }
+}
+
+stage('Test') {
+    def allTests = [:]
+    def base_dir = pwd()
+    allTests['chrome'] = functional_test('chrome', base_dir)
+    allTests['firefox'] = functional_test('firefox', base_dir)
+    allTests['headless'] = headless_test(base_dir)
+    parallel allTests
 }
